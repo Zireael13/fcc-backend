db92c5091e0a7dc8338d88d40b87c34594cb5f85
Matt Wilkinson <mattwilki17@gmail.com>
2020-09-05 15:49:03 -0400

    init

---
+++ myApp.js
@@ -0,0 +1,83 @@
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// --> 7)  Mount the Logger middleware here
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+})
+
+// --> 11)  Mount the body-parser middleware  here
+app.use(bodyParser.urlencoded({extended: false}));
+
+/** 1) Meet the node console. */
+console.log("Hello World");
+
+/** 2) A first working Express Server */
+app.get('/', (req, res) => {
+  const absolutePath = __dirname + '/views/index.html'
+        res.sendFile(absolutePath);
+        });
+
+/** 3) Serve an HTML file */
+
+
+/** 4) Serve static assets  */
+app.use(express.static(__dirname + '/public'))
+
+/** 5) serve JSON on a specific route */
+app.get('/json', (req, res) => {
+  res.json({
+    'message': process.env.MESSAGE_STYLE == 'uppercase' ?  'HELLO JSON' : 'Hello json'
+  });
+});
+
+/** 6) Use the .env file to configure the app */
+ 
+ 
+/** 7) Root-level Middleware - A logger */
+//  place it before all the routes !
+
+
+/** 8) Chaining middleware. A Time server */
+app.get('/now', (req, res, next) => {
+  const currentTime = new Date().toString();
+  req.time = currentTime;
+  next();
+}, (req, res) => {
+  res.send({time: req.time});
+});
+
+
+/** 9)  Get input from client - Route parameters */
+app.get('/:word/echo', (req, res) => {
+  res.send({echo: req.params.word});
+});
+
+/** 10) Get input from client - Query parameters */
+// /name?first=<firstname>&last=<lastname>
+app.get('/name', (req, res) => {
+  res.send({name: `${req.query.first} ${req.query.last}`})
+})
+
+  
+/** 11) Get ready for POST Requests - the `body-parser` */
+// place it before all the routes !
+
+
+/** 12) Get data form POST  */
+app.post('/name',(req, res) => {
+  res.send({name :`${req.body.first} ${req.body.last}`});
+});
+
+
+
+// This would be part of the basic setup of an Express app
+// but to allow FCC to run tests, the server is already active
+/** app.listen(process.env.PORT || 3000 ); */
+
+//---------- DO NOT EDIT BELOW THIS LINE --------------------
+
+ module.exports = app;
